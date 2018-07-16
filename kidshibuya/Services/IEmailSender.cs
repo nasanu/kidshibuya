1638f9dcc7c3f359e21c7b01e339f7cf29c801f9
Nathan Brown <kidshibuya@gmail.com>
2018-07-16 22:43:46 +0900

    added fb login

---
+++ kidshibuya/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kidshibuya.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
